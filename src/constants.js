98e88ffcc88ff5d6da469efaacb7f85a2fc3efee
shj <kan02134@gmail.com>
2021-06-05 07:54:00 +0900

    fix: change DEFAULT_FONT_SIZE type to string
    - and add comments

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,14 @@
 export default {
-  DEFAILT_FONT_SIZE: 12,
+  /**
+   * allow string type value in input element
+   */
+  DEFAILT_FONT_SIZE: '12',
+  /**
+   * allow number type value in input element
+   */
   MAX_FONT_SIZE: 80,
+  /**
+   * allow number type value in input element
+   */
   MIN_FONT_SIZE: 10,
 };
