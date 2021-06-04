5e77a6a9f7f21546fde09f2c67f52374e64991ec
shj <kan02134@gmail.com>
2021-06-05 04:18:20 +0900

    feat: implement about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
+import About from '../pages/About/About.vue';
+
 export default createRouter({
   history: createWebHashHistory(),
-  routes: [],
+  routes: [
+    {
+      path: '/about',
+      component: About,
+    },
+  ],
 });
