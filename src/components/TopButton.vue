9c2c2a0704f7058291ffdb2830c3da7fcff5f563
shj <kan02134@gmail.com>
2021-06-05 02:08:07 +0900

    feat: implement top-button

---
+++ src/components/TopButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="top-button">
+    <span class="top-button__icon material-icons-outlined">arrow_upward</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.top-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48px;
+  height: 48px;
+  padding: 12px;
+  border-radius: 50%;
+  background-color: var(--top-button-background-color);
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.25);
+  user-select: none;
+  cursor: pointer;
+
+  &__icon {
+    color: var(--text-color);
+  }
+}
+</style>
