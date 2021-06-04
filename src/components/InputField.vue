07b0b87752e7ad8f5fd7e86037f03e7cd147ae60
shj <kan02134@gmail.com>
2021-06-04 16:07:19 +0900

    feat: implement input-field

---
+++ src/components/InputField.vue
@@ -0,0 +1,22 @@
+<template>
+  <input class="input-field__input typo-text" :placeholder="placeholder" />
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const { placeholder } = defineProps({
+  placeholder: {
+    type: String,
+    default: '무엇이든 입력해보세요',
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+input {
+  width: 100%;
+  border: none;
+  outline: none;
+}
+</style>
