c40f16d0fc40802e33ddff315740b8ab7d6dc2ce
shj <kan02134@gmail.com>
2021-06-04 16:49:30 +0900

    refactor: remove useless component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main>
+    <p class="typo-text">app</p>
+  </main>
 </template>
-
-<script setup>
-import HelloWorld from './components/HelloWorld.vue';
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
 
 <style lang="scss" scoped>
 main {
