0f906f37509ab683b05e095a13158217a0acc971
shj <kan02134@gmail.com>
2021-06-05 03:06:13 +0900

    feat: implement header-panel

---
+++ src/pages/share/HeaderPanel.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <app-title />
+    <theme-button />
+  </header>
+</template>
+
+<script setup>
+import AppTitle from '../../components/AppTitle.vue';
+import ThemeButton from '../../components/ThemeButton.vue';
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid var(--border-color);
+}
+</style>
