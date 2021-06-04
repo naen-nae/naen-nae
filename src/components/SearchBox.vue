2fa2030ce56a68e59523fa55e959fe5d0d856de8
shj <kan02134@gmail.com>
2021-06-04 16:07:30 +0900

    feat: implement search-box component

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="search-box">
+    <span class="search-box__icon material-icons-outlined">search</span>
+    <input-field placeholder="이름으로 폰트 검색하기" />
+  </div>
+</template>
+
+<script setup>
+import InputField from './InputField.vue';
+</script>
+
+<style lang="scss" scoped>
+.search-box {
+  display: flex;
+
+  &__icon {
+    user-select: none;
+    margin-right: 12px;
+  }
+}
+</style>
