e03e22a351f27109b25975a09752aa49f3aa887d
shj <kan02134@gmail.com>
2021-06-07 04:19:11 +0900

    feat: implement home page

---
+++ src/pages/Home/CardBox/CardBoxHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card-box-header">
+    <p class="card-box-header__title typo-title" v-text="fontFamily" />
+    <div class="card-box-header__subtitle">
+      <p
+        class="card-box-header__subtitle--author typo-subtitle"
+        v-text="author"
+      />
+      <p class="typo-subtitle">{{ stylesLen }} Styles</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  fontFamily: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  stylesLen: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.card-box-header {
+  display: flex;
+  flex-direction: column;
+
+  &__title {
+    word-break: break-all;
+  }
+
+  &__subtitle {
+    display: flex;
+    margin-top: 8px;
+    justify-content: space-between;
+
+    &--author {
+      color: var(--subtitle-color);
+    }
+  }
+}
+</style>
