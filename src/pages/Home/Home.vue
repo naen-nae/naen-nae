5cb3d60c9d4a2ea13a23911c909e1c9faaa0dc3e
shj <kan02134@gmail.com>
2021-06-07 04:19:11 +0900

    feat: implement home page

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <modifier-bar v-model:search-value="search" />
+    <section class="home__cards">
+      <card-box
+        v-for="(font, ind) in filteredFonts"
+        :key="`${font.fontFamily}-${ind}`"
+        :font="font"
+        @click="
+          router.push({
+            name: 'detail',
+            params: { fontName: font.fontFamily },
+          })
+        "
+      />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import ModifierBar from './ModifierBar.vue';
+import CardBox from './CardBox/CardBox.vue';
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+import { computed, ref, toRefs } from 'vue';
+
+const store = useStore();
+const { fonts } = toRefs(store.state);
+
+const router = useRouter();
+
+const search = ref('');
+const filteredFonts = computed(() =>
+  fonts.value.filter(({ fontFamily }) =>
+    RegExp(search.value.toLowerCase()).test(fontFamily.toLowerCase()),
+  ),
+);
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/display.scss';
+
+.home {
+  align-self: flex-start;
+  width: 100%;
+
+  &__cards {
+    display: grid;
+    margin-top: 38px;
+    grid-template-columns: repeat(auto-fill, minmax(30%, auto));
+    row-gap: 12px;
+    column-gap: 16px;
+
+    @include mobile {
+      grid-template-columns: minmax(100%, auto);
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 import About from '../pages/About/About.vue';
+import Home from '../pages/Home/Home.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -8,6 +9,14 @@ export default createRouter({
     {
       path: '/about',
       component: About,
+    },
+    {
+      path: '/',
+      component: Home,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: Home,
     },
   ],
 });
