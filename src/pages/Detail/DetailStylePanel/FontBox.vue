935e0932decd7aece2d94045d6ba5f6a9421e640
shj <kan02134@gmail.com>
2021-06-07 18:41:02 +0900

    feat: implement detail-style panel

---
+++ src/pages/Detail/DetailStylePanel/FontBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="font-box">
+    <p class="font-box__style typo-text" v-text="fontWeight" />
+    <div class="font-box__contents">
+      <p
+        :style="{ fontSize: `${fontSize}px`, fontFamily, fontWeight }"
+        v-text="textContent !== '' ? textContent : fontWeight"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const { textContent, fontSize } = toRefs(store.state);
+
+defineProps({
+  fontWeight: {
+    type: String,
+    required: true,
+  },
+  fontFamily: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.font-box {
+  &__style {
+    color: var(--subtitle-color);
+  }
+
+  &__contents {
+    word-break: break-all;
+    color: var(--text-color);
+  }
+}
+</style>
