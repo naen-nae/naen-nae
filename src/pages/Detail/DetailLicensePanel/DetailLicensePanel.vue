55d7219c72939b037c2137cefee96a45328a62d1
shj <kan02134@gmail.com>
2021-06-07 21:55:41 +0900

    feat: implement detail-license panel

---
+++ src/pages/Detail/DetailLicensePanel/DetailLicensePanel.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="detail-license">
+    <p class="typo-title">라이선스</p>
+
+    <div class="detail-license__full">
+      <p class="typo-subtitle">라이선스 전문</p>
+      <pre class="typo-text" v-html="parsedLicense" />
+    </div>
+
+    <div class="detail-license__summary">
+      <p class="typo-subtitle">라이선스 요약</p>
+      <summary-table :summary="licenseSummary" />
+      <p class="typo-text">
+        * 위 내용은 참고용으로, 정확한 사용 범위는 폰트 제작자/제작사에 확인
+        바랍니다.
+        <br />
+        사용 범위는 폰트 제작사의 세부 규정에 따라 달라질 수 있으며, 낸내는 폰트
+        소프트웨어 사용과 취급으로 발생하는 일에 대해 책임 및 의무를 가지지
+        않습니다.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import SummaryTable from './SummaryTable.vue';
+
+const { license, licenseSummary } = defineProps({
+  license: {
+    type: String,
+    required: true,
+  },
+  licenseSummary: {
+    type: Object,
+    required: true,
+  },
+});
+
+const parsedLicense = license.replace(
+  /(https?:.*)(\s|$)/,
+  '<a href="$1" target="_blank">$1</a>',
+);
+</script>
+
+<style lang="scss" scoped>
+.detail-license {
+  & > * {
+    margin-bottom: 18px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .typo-subtitle {
+    font-weight: bold;
+    margin-bottom: 8px;
+  }
+
+  &__full > .typo-text {
+    white-space: pre-wrap;
+  }
+
+  &__summary > .typo-text {
+    margin-top: 12px;
+    color: var(--subtitle-color);
+  }
+}
+</style>
---
+++ src/pages/Detail/DetailLicensePanel/SummaryTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th
+          v-for="(header, ind) in headers"
+          :key="`${header}-${ind}`"
+          class="typo-text"
+          v-text="header"
+        />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="([category, description, allow], ind) in bodies"
+        :key="`${category}-${ind}`"
+      >
+        <td class="typo-text" v-text="category" />
+        <td class="typo-text" v-text="description" />
+        <td class="typo-text" v-text="allow ? 'O' : 'X'" />
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import zip from 'lodash/zip';
+
+const { summary } = defineProps({
+  summary: {
+    type: Object,
+    required: true,
+  },
+});
+
+const categories = [
+  '인쇄',
+  '웹사이트',
+  '영상',
+  '포장지',
+  '임베딩',
+  'BI/CI',
+  'OFL',
+];
+const descriptions = [
+  '브로슈어, 포스터, 책, 잡지 및 출판용 인쇄물 등',
+  '웹페이지, 광고 배너, 메일, E-브로슈어 등',
+  '영상물 자막, 영화 오프닝/엔딩 크레딧, UCC 등',
+  '판매용 상품의 패키지',
+  '웹사이트 및 프로그램 서버 내 폰트 탑재, E-book 제작',
+  '회사명, 브랜드명, 상품명, 로고, 마크, 슬로건, 캐치프레이즈',
+  '폰트 파일의 수정/ 복제/ 배포 가능. 단, 폰트 파일의 유료 판매는 금지',
+];
+
+const headers = ['카테고리', '사용 범위', '허용 여부'];
+const bodies = zip(categories, descriptions, Object.values(summary));
+</script>
+
+<style lang="scss" scoped>
+table {
+  width: 100%;
+  text-align: center;
+
+  td {
+    padding: 6px;
+  }
+}
+</style>
