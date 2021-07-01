8e30a3c6e5fad01bbe44691d524b72da6c51157c
shj <kan02134@gmail.com>
2021-07-01 21:01:15 +0900

    fix: fix subset-font-css logic

--- scripts/fontGen/genSubsetFonts.js
+++ scripts/fontGen/genSubsetFonts.js
@@ -2,46 +2,11 @@ const fs = require('fs/promises');
 const _ = require('lodash');
 const subsetFont = require('subset-font');
 
-const toCdnSrc = require('./toCdnSrc');
-const getFaces = require('./getFaces');
 const genNewDir = require('./genNewDir');
 const getFontsObj = require('./getFontsObj');
 
 // gen subset-fonts
 (async () => {
-  // gen font-faces
-  const fonts = _.map(await getFontsObj(), font =>
-    _.set(
-      font,
-      'files',
-      _.map(font.files, path => toCdnSrc(`subset-fonts/${path}`, 'gh-pages')),
-    ),
-  );
-
-  const faceOptions = _.flatMap(fonts, ({ files, fontWeights, fontFamily }) =>
-    _.map(_.zip(files, fontWeights), ([src, fontWeight]) => ({
-      src: `${src}.woff2`,
-      fontWeight,
-      fontFamily,
-      format: 'woff2',
-    })),
-  );
-
-  const faceOptionsGroupByFamily = _.groupBy(
-    faceOptions,
-    ({ fontFamily }) => fontFamily,
-  );
-
-  const facesByFamily = _.map(faceOptionsGroupByFamily, (options, name) => [
-    name,
-    getFaces(options),
-  ]);
-
-  await genNewDir('subset-css');
-  _.forEach(facesByFamily, ([name, css]) =>
-    fs.writeFile(`./public/subset-css/${name}.css`, css),
-  );
-
   // gen subset font files
   await genNewDir('subset-fonts');
   _.map(await getFontsObj(), async ({ files: filePaths }, ind, { length }) => {
