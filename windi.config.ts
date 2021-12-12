540fb31a4b2c58195c8a9aab6f1821f7c13a5b01
Gumball12 <to@shj.rip>
2021-12-12 21:10:03 +0900

    feat: add typo shortcuts

--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,7 @@
+import { defineConfig } from '@windicss/plugin-utils';
 import defaultTheme from 'windicss/defaultTheme';
 
-export default {
+export default defineConfig({
   theme: {
     extend: {
       fontFamily: {
@@ -8,5 +9,13 @@ export default {
       },
     },
   },
+  shortcuts: {
+    'typo-headline': 'dark:text-gray-50 text-3xl tracking-wider',
+    'typo-title': 'dark:text-gray-50 text-2xl tracking-wider',
+    'typo-subtitle': 'dark:text-gray-50 text-lg tracking-wide',
+    'typo-text': 'dark:text-gray-50 text-sm tracking-wide',
+    'typo-link':
+      'dark:text-blue-300 text-sm tracking-wide font-bold cursor-pointer hover:underline',
+  },
   attributify: true,
-};
+});
