1d4a1939381ffb6117d07e1fc0772ca08de0af65
Gumball12 <to@shj.rip>
2021-12-12 14:48:11 +0900

    feat: implement theme button component

---
+++ src/components/header/ThemeButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <IconButton :class="{ turn: isDark }" @click="toggleDark()">{{
+    isDark ? 'brightness_low' : 'brightness_high'
+  }}</IconButton>
+</template>
+
+<script lang="ts" setup>
+import { useDark, useToggle } from '@vueuse/core';
+import IconButton from 'components/IconButton.vue';
+
+const isDark = useDark({
+  attribute: 'color-scheme',
+  valueDark: 'dark',
+});
+const toggleDark = useToggle(isDark);
+</script>
+
+<style lang="scss" scoped>
+.turn {
+  transform: rotate(360deg);
+}
+</style>
