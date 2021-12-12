361b595840a673f22456b7c94968cda64af2398e
Gumball12 <to@shj.rip>
2021-12-12 16:45:00 +0900

    chore: add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'prettier', 'plugin:storybook/recommended'],
 };
---
+++ .storybook/main.js
@@ -0,0 +1,34 @@
+const pagesPlugin = require('vite-plugin-pages');
+
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+  ],
+  core: { builder: 'storybook-builder-vite' },
+  viteFinal: async config => {
+    config.resolve.alias = [
+      {
+        find: 'src',
+        replacement: '/src/',
+      },
+      {
+        find: 'components',
+        replacement: '/src/components/',
+      },
+      {
+        find: 'pages',
+        replacement: '/src/pages/',
+      },
+      {
+        find: 'vue',
+        replacement: 'vue/dist/vue.esm-bundler.js',
+      },
+    ];
+
+    config.plugins.push(pagesPlugin.default({ extensions: ['vue'] }));
+    return config;
+  },
+};
