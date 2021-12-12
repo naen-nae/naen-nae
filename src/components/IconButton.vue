ce17d2afe8cac5d371748bb195a29add36e604a9
Gumball12 <to@shj.rip>
2021-12-12 14:44:22 +0900

    feat: implement IconButton component

---
+++ src/components/IconButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="icon material-icons-outlined"><slot></slot></span>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+  -webkit-tap-highlight-color: transparent;
+  cursor: pointer;
+  transition: transform 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+  user-select: none;
+}
+</style>
