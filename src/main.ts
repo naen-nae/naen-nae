99ff4d0970a948664dbcd36489b624712a5cec22
Gumball12 <to@shj.rip>
2021-12-12 14:45:50 +0900

    feat: implement theme style

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+/* eslint-disable */
+
 import { ViteSSG } from 'vite-ssg';
 import App from './App.vue';
+import './styles/index.scss';
 import routes from 'virtual:generated-pages';
 
 export const createApp = ViteSSG(App, { routes });
