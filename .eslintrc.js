e70eddd57c150b8488b91b75184492064a2c418a
Gumball12 <to@shj.rip>
2021-12-12 17:29:01 +0900

    chore(lint): set import/no-extraneous-dependencies rule to off

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-base', 'prettier', 'plugin:storybook/recommended'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+  },
 };
