8cd22d7edb0f5acc69f9dcb4bda7ca006e66b318
Gumball12 <to@shj.rip>
2021-12-12 14:47:44 +0900

    feat: use theme color

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -5,6 +5,7 @@
 <style lang="scss" scoped>
 .icon {
   -webkit-tap-highlight-color: transparent;
+  color: var(--text-color);
   cursor: pointer;
   transition: transform 0.3s cubic-bezier(1, 0.5, 0.8, 1);
   user-select: none;
