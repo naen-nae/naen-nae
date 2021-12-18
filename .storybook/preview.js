a771550955223db570a37bb5cdd80624230592c5
Gumball12 <to@shj.rip>
2021-12-18 14:17:29 +0900

    chore: use pinia

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,11 @@
 import '../src/styles/index.scss';
+import { app } from '@storybook/vue3';
 import Container from './Container.vue';
 import 'windi.css';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia();
+app.use(pinia);
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 /* eslint-disable */
 
+import devalue from '@nuxt/devalue';
 import { ViteSSG } from 'vite-ssg';
 import App from './App.vue';
 import './styles/index.scss';
 import 'virtual:windi.css';
 import routes from 'virtual:generated-pages';
+import { createPinia } from 'pinia';
 
-export const createApp = ViteSSG(App, { routes });
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  ({ app, initialState }) => {
+    const pinia = createPinia();
+    app.use(pinia);
+
+    if (import.meta.env.SSR) {
+      initialState.pinia = pinia.state.value;
+    } else {
+      pinia.state.value = initialState?.pinia ?? {};
+    }
+  },
+  {
+    transformState: state => (import.meta.env.SSR ? devalue(state) : state),
+  },
+);
