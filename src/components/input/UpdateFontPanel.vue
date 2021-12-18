21f25ba16c05abd8aef6fbce9f771e9c21ef6552
Gumball12 <to@shj.rip>
2021-12-18 22:20:20 +0900

    feat: implement UpdateFontPanel component

---
+++ src/components/input/UpdateFontPanel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="update-font-panel border border-true-gray-200 flex h-[48px] justify-between rounded-full px-[12px]"
+    dark="border-true-gray-500"
+  >
+    <SearchPanel class="w-full" />
+    <UpdateFontSizePanel class="w-full <md:hidden" />
+    <ResetButton class="<md:hidden" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import SearchPanel from './SearchPanel.vue';
+import UpdateFontSizePanel from './UpdateFontSizePanel.vue';
+import ResetButton from '../buttons/ResetButton.vue';
+</script>
+
+<style lang="scss" scoped>
+.update-font-panel {
+  & > *::before {
+    @apply bg-true-gray-200 dark:bg-true-gray-500 h-[48px] mx-[12px] w-[1px];
+    content: ' ';
+  }
+
+  & > *:first-child::before {
+    @apply h-0 w-0 mx-0;
+    content: '';
+  }
+}
+</style>
---
+++ src/stories/UpdateFontPanel.stories.js
@@ -0,0 +1,11 @@
+import UpdateFontPanel from 'components/input/UpdateFontPanel.vue';
+
+export default {
+  title: 'components/input/UpdateFontPanel',
+  component: UpdateFontPanel,
+};
+
+export const Default = () => ({
+  components: { UpdateFontPanel },
+  template: `<UpdateFontPanel />`,
+});
