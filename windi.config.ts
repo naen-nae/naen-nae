958d5fc381cd0f567b6f470af3c14f30fac7e3f9
Gumball12 <to@shj.rip>
2021-12-18 23:07:51 +0900

    feat: update typo font size

--- windi.config.ts
+++ windi.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
     ),
   ],
   shortcuts: {
-    'typo-headline': 'dark:text-gray-50 text-3xl tracking-wider',
-    'typo-title': 'dark:text-gray-50 text-2xl tracking-wider',
-    'typo-subtitle': 'dark:text-gray-50 text-lg tracking-wide',
-    'typo-text': 'dark:text-gray-50 text-sm tracking-wide',
+    'typo-headline': 'dark:text-gray-50 text-[32px] tracking-wider',
+    'typo-title': 'dark:text-gray-50 text-[24px] tracking-wider',
+    'typo-subtitle': 'dark:text-gray-50 text-[14px] tracking-wide',
+    'typo-text': 'dark:text-gray-50 text-[12px] tracking-wide',
     'typo-link':
-      'text-blue-600 dark:text-blue-300 text-sm tracking-wide font-bold cursor-pointer hover:underline',
+      'text-blue-600 dark:text-blue-300 text-[12px] tracking-wide font-bold cursor-pointer hover:underline',
     'flex-center': 'flex justify-center items-center',
   },
   attributify: true,
