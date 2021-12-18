6b30c3380d93d295501e6bdf0c2ea7244417c8b2
Gumball12 <to@shj.rip>
2021-12-18 20:35:25 +0900

    feat: add font size state

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_FONT_SIZE = '33';
+export const MAX_FONT_SIZE = 80;
+export const MIN_FONT_SIZE = 8;
--- src/store/font.ts
+++ src/store/font.ts
@@ -1,10 +1,14 @@
+import { DEFAULT_FONT_SIZE } from 'src/constants';
+
 interface FontState {
   search: string;
+  size: string;
 }
 
 export const useFontStore = defineStore({
   id: 'font',
   state: (): FontState => ({
     search: '',
+    size: DEFAULT_FONT_SIZE,
   }),
 });
