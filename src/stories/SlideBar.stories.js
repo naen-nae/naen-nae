68e48ab14804c7e17e602caadfdf5495419cc9a3
Gumball12 <to@shj.rip>
2021-12-18 16:47:43 +0900

    test: add min, max args

--- src/stories/SlideBar.stories.js
+++ src/stories/SlideBar.stories.js
@@ -3,6 +3,20 @@ import SlideBar from 'components/input/SlideBar.vue';
 export default {
   title: 'components/input/SlideBar',
   component: SlideBar,
+  argTypes: {
+    min: {
+      control: {
+        type: 'number',
+        min: 1,
+        max: 10,
+      },
+    },
+    max: {
+      control: 'number',
+      min: 11,
+      max: 100,
+    },
+  },
 };
 
 const Template = args => ({
@@ -13,3 +27,7 @@ const Template = args => ({
 });
 
 export const Default = Template.bind({});
+Default.args = {
+  min: 1,
+  max: 100,
+};
