039cf67b23932a8aa49116338cdab249ef103edc
Gumball12 <to@shj.rip>
2021-12-19 15:03:54 +0900

    feat: implement CardPanel component

---
+++ src/components/cards/CardPanel.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    class="rounded-md cursor-pointer shadow-transparent min-h-[141px] p-[12px] transform-gpu translate-y-0 overflow-hidden"
+    border="~ gray-color"
+    transition="~ duration-300"
+    hover="shadow-lg -translate-y-0.5"
+  >
+    <div class="flex flex-col">
+      <p class="typo-title" v-text="font.name" />
+      <div class="flex mt-[8px] justify-between typo-subtitle">
+        <p
+          class="text-true-gray-400 dark:text-true-gray-400"
+          v-text="font.author"
+        />
+        <p>{{ font.files.length }} 스타일</p>
+      </div>
+    </div>
+    <hr />
+    <div
+      class="break-all overflow-y-hidden overflow-x-scroll"
+      dark="text-gray-50"
+    >
+      <transition name="fade-up">
+        <p
+          v-show="font.availableSubset"
+          class="leading-tight overflow-visible whitespace-nowrap"
+          :style="{ fontSize: `${size}px`, fontFamily: font.family }"
+        >
+          다람쥐 헌 쳇바퀴에 타고파 abcdefghijklmnopqrstuvwxyz 0123456789
+          !@#$%^&*()_+=-
+        </p>
+      </transition>
+      <p v-if="!font.availableSubset" class="typo-subtitle">
+        폰트를 불러오고 있어요...
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useFontStore } from 'src/store/font';
+import { Font } from 'src/types/Font';
+
+defineProps<{
+  font: Font;
+}>();
+
+const store = useFontStore();
+const { size } = storeToRefs(store);
+</script>
+
+<style lang="scss" scoped>
+.fade-up {
+  &-enter-active,
+  &-leave-active {
+    @apply transform-gpu transition translate-y-0 duration-500;
+  }
+
+  &-enter-from,
+  &-leave-to {
+    @apply opacity-0 translate-y-[10px];
+  }
+}
+</style>
---
+++ src/stories/CardPanel.stories.js
@@ -0,0 +1,52 @@
+import CardPanel from 'components/cards/CardPanel.vue';
+import { useFontStore } from 'src/store/font';
+
+export default {
+  title: 'components/cards/CardPanel',
+  component: CardPanel,
+};
+
+export const Default = args => ({
+  components: { CardPanel },
+  setup: () => {
+    const font = reactive(args.font);
+    setTimeout(() => {
+      font.availableSubset = true;
+    }, 2000);
+
+    const fontStore = useFontStore();
+    const { size } = storeToRefs(fontStore);
+    const setRandomSize = () => {
+      size.value = Math.ceil(Math.random() * 50 + 30);
+    };
+
+    return { font, size, setRandomSize };
+  },
+  template: `
+    <CardPanel :font="font" />
+    <hr />
+    <p>font size: {{ size }}</p>
+    <button style="background-color: white; border: 2px solid; padding: 0.3em;" @click="setRandomSize">set random font size</button>
+  `,
+});
+
+Default.args = {
+  font: {
+    name: 'font name',
+    author: 'author',
+    family: 'sans-serif',
+    files: ['a.ttf', 'b.ttf', 'c.ttf'],
+    weights: [400, 600, 800],
+    licenseSummary: {
+      print: true,
+      website: true,
+      video: true,
+      paper: true,
+      embeding: true,
+      bici: true,
+      ofl: false,
+    },
+    license: 'LICENSE',
+    availableSubset: false,
+  },
+};
