1578902bc72be879f77543e35fb61ec3c86e4709
Gumball12 <to@shj.rip>
2021-12-19 22:37:04 +0900

    feat: add will-change-transform class

--- windi.config.ts
+++ windi.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
         '.break-keep': {
           'word-break': 'keep-all',
         },
+        '.will-change-transform': {
+          willChange: 'transform',
+        },
       }),
     ),
   ],
