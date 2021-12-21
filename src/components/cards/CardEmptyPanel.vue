d4b287330de292310941515c1cb6416e89989004
Gumball12 <to@shj.rip>
2021-12-22 00:30:53 +0900

    feat: implement CardEmptyPanel component

---
+++ src/components/cards/CardEmptyPanel.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col h-64 w-full gap-y-3 items-center justify-center">
+    <p class="text-transparent text-shadow-xl text-5xl dark:text-shadow-darkXl">
+      X ~ X
+    </p>
+  </div>
+</template>
---
+++ src/stories/CardEmptyPanel.stories.js
@@ -0,0 +1,11 @@
+import CardEmptyPanel from 'components/cards/CardEmptyPanel.vue';
+
+export default {
+  title: 'components/cards/CardEmptyPanel',
+  component: CardEmptyPanel,
+};
+
+export const Default = () => ({
+  components: { CardEmptyPanel },
+  template: '<CardEmptyPanel />',
+});
