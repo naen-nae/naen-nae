eed2a702871b62938f708765dd31784947a915fb
Gumball12 <to@shj.rip>
2021-12-22 13:40:46 +0900

    test: fix Container style

--- .storybook/Container.vue
+++ .storybook/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-[100vh] dark:bg-true-gray-800">
+  <div class="container">
     <slot />
     <ThemeButton
       style="position: fixed; left: 1em; bottom: 1em; z-index: 999"
@@ -10,6 +10,17 @@
 <script lang="ts" setup>
 import ThemeButton from 'components/nav/ThemeButton.vue';
 </script>
+
+<style lang="scss" scoped>
+html .container {
+  background-color: #fff;
+  min-height: 100vh;
+}
+
+html.dark .container {
+  background-color: rgba(38, 38, 38, 1);
+}
+</style>
 
 <style lang="scss">
 body {
