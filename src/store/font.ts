b68d17535a03adab8e9ae7e71c68c296945b3ea9
Gumball12 <to@shj.rip>
2021-12-22 23:47:14 +0900

    feat: add fonts state

--- src/store/font.ts
+++ src/store/font.ts
@@ -1,8 +1,10 @@
 import { DEFAULT_FONT_SIZE } from 'src/constants';
+import { Font } from 'src/types/Font';
 
 interface FontState {
   search: string;
   size: string;
+  fonts: Font[];
 }
 
 export const useFontStore = defineStore({
@@ -10,5 +12,6 @@ export const useFontStore = defineStore({
   state: (): FontState => ({
     search: '',
     size: DEFAULT_FONT_SIZE,
+    fonts: [],
   }),
 });
