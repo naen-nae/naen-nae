c8bebe8730894c36282b881f1710205041842402
Gumball12 <to@shj.rip>
2021-12-22 23:13:43 +0900

    feat: makes emit the close event when click the close icon

--- src/components/snackbar/SnackbarListItem.vue
+++ src/components/snackbar/SnackbarListItem.vue
@@ -3,7 +3,9 @@
     class="rounded-md bg-true-gray-700 min-h-[38px] min-w-24 py-1 px-3 inline-flex justify-between items-center"
   >
     <p class="mr-4 text-white typo-subtitle"><slot /></p>
-    <IconButton class="text-white text-size-[18px]">close</IconButton>
+    <IconButton class="text-white text-size-[18px]" @click="$emit('close')"
+      >close</IconButton
+    >
   </div>
 </template>
 
