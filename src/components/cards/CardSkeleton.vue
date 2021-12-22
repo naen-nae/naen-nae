f385c2142c2987aab16485360e1e48853caf5ad3
Gumball12 <to@shj.rip>
2021-12-23 01:00:07 +0900

    feat: use animate-pulse for root element

--- src/components/cards/CardSkeleton.vue
+++ src/components/cards/CardSkeleton.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="border-gray-color border rounded-md p-[12px] card-panel">
+  <div
+    class="border-gray-color border rounded-md p-[12px] animate-pulse card-panel"
+  >
     <div class="flex justify-between">
-      <span class="h-4 animate-pulse w-3/5 skeleton-span" />
-      <span class="h-4 animate-pulse w-1/6 skeleton-span" />
+      <span class="h-4 w-3/5 skeleton-span" />
+      <span class="h-4 w-1/6 skeleton-span" />
     </div>
     <hr />
-    <div class="h-8 animate-pulse w-1/2 skeleton-span" />
+    <div class="h-8 w-1/2 skeleton-span" />
   </div>
 </template>
 
