91070ba2c843ed2094a23399fe92a3189fdf6aad
Gumball12 <to@shj.rip>
2021-12-26 20:28:54 +0900

    feat: add components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
-  <router-view />
+  <SnackbarList />
+
+  <main
+    class="flex flex-col min-h-[100vh] px-6 transition main justify-between dark:bg-true-gray-800"
+  >
+    <TheHeader />
+    <div class="py-4 px-6 <md:p-3">
+      <router-view />
+    </div>
+
+    <TheFooter />
+  </main>
+
+  <TopButton />
 </template>
+
+<script lang="ts" setup>
+import SnackbarList from 'components/snackbar/SnackbarList.vue';
+import TheFooter from 'components/section/TheFooter.vue';
+import TheHeader from 'components/section/TheHeader.vue';
+import TopButton from './components/buttons/TopButton.vue';
+</script>
+
+<style lang="scss" scoped>
+.main {
+  transition-property: background-color;
+}
+</style>
