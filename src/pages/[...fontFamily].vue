927a34be8033bc0ac8896f2c914e9024e6d14645
Gumball12 <to@shj.rip>
2021-12-26 20:02:29 +0900

    feat: implement Detail page

---
+++ src/pages/[...fontFamily].vue
@@ -0,0 +1,78 @@
+<template>
+  <template v-if="isInitialized">
+    <template v-if="!font">
+      <PageNotFound />
+      <p class="text-center typo-subtitle">폰트를 찾을 수 없어요 ...</p>
+    </template>
+
+    <div v-else>
+      <DetailTitle :font="font" />
+      <hr />
+      <DetailFont :font="font" />
+      <hr />
+      <DetailCode :font="font" />
+      <hr />
+      <DetailLicense :font="font" />
+    </div>
+  </template>
+
+  <div v-else class="flex flex-col animate-pulse gap-y-4">
+    <div class="h-8 w-1/4 skeleton-span" />
+    <div class="h-8 w-1/2 skeleton-span" />
+    <hr />
+    <div class="flex gap-x-3">
+      <div class="h-8 w-1/8 skeleton-span <sm:hidden" />
+      <div class="h-8 w-1/2 skeleton-span" />
+    </div>
+    <div class="h-8 w-1/2 skeleton-span" />
+    <div class="h-8 w-1/3 skeleton-span" />
+    <hr />
+    <div class="h-8 w-1/8 skeleton-span <sm:w-3/8" />
+    <div class="h-8 w-1/2 skeleton-span" />
+    <div class="h-8 w-1/3 skeleton-span" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useFontStore } from 'src/store/font';
+import PageNotFound from 'src/components/section/PageNotFound.vue';
+import DetailTitle from 'src/components/section/DetailTitle.vue';
+import DetailFont from 'src/components/section/DetailFont.vue';
+import DetailCode from 'src/components/section/DetailCode.vue';
+import DetailLicense from 'src/components/section/DetailLicense.vue';
+import { useTitleMeta } from 'src/composables/head';
+import { META_DESCRIPTION, META_TITLE } from 'src/constants';
+
+const store = useFontStore();
+const { fonts, isInitialized } = storeToRefs(store);
+
+const {
+  fontFamily: [fontFamily],
+} = defineProps<{
+  fontFamily: string[];
+}>();
+
+const font = computed(() =>
+  fonts.value.find(
+    ({ fontFamily: targetFontFamily }) => targetFontFamily === fontFamily,
+  ),
+);
+
+const fontName = computed(() => font.value?.name ?? '폰트를 찾을 수 없어요');
+const metaTitle = computed(() => `${fontName.value} :: ${META_TITLE}`);
+const metaDescription = computed(
+  () => `${fontName.value} :: ${META_DESCRIPTION}`,
+);
+
+useTitleMeta({
+  title: metaTitle,
+  description: metaDescription,
+  url: fontFamily,
+});
+</script>
+
+<style lang="scss" scoped>
+hr {
+  @apply my-7;
+}
+</style>
--- src/stories/DetailPage.stories.js
+++ src/stories/DetailPage.stories.js
@@ -1,11 +1,53 @@
-import DetailPage from 'src/pages/detail.vue';
+import DetailPage from 'src/pages/[...fontFamily].vue';
+import { useFontStore } from 'src/store/font';
 
 export default {
   title: 'pages/DetailPage',
   component: DetailPage,
 };
 
-export const Default = () => ({
+export const Default = ({ fontFamily }) => ({
   components: { DetailPage },
-  template: '<DetailPage />',
+  setup: () => {
+    const fontStore = useFontStore();
+    fontStore.initialize();
+
+    return { fontFamily };
+  },
+  template: '<DetailPage :fontFamily="fontFamily" />',
 });
+
+Default.args = {
+  fontFamily: ['esamanru'],
+};
+
+export const NotFound = ({ fontFamily }) => ({
+  components: { DetailPage },
+  setup: () => {
+    const fontStore = useFontStore();
+    fontStore.initialize();
+
+    return { fontFamily };
+  },
+  template: '<DetailPage :fontFamily="fontFamily" />',
+});
+
+NotFound.args = {
+  fontFamily: ['no-exists-font-name'],
+};
+
+export const Loading = ({ fontFamily }) => ({
+  components: { DetailPage },
+  setup: () => {
+    const fontStore = useFontStore();
+    const { fonts } = storeToRefs(fontStore);
+    fonts.value = [];
+
+    return { fontFamily };
+  },
+  template: '<DetailPage :fontFamily="fontFamily" />',
+});
+
+Loading.args = {
+  fontFamily: ['not-important-font-name'],
+};
